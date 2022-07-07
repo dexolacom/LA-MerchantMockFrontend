53fba181a58abedd55f841de0d96dc4b65c94710
happy-cutman <happy.cutman@gmail.com>
2022-07-07 10:54:24 +0300

    ADD | frame logic

--- src/components/frame/index.ts
+++ src/components/frame/index.ts
@@ -1,14 +1,33 @@
+import { frameStyles } from './styles';
 
 
 export const showFrame = () => {
+  if (document.getElementById('frame-modal')) {
+    return
+  }
   const modal = document.createElement('div')
   modal.innerHTML = `
-    <div class='backdrop'>
-      <div class='modal'>
-        <span>Hello world</span>
-      </div>
+    <div class='frame-modal' id='frame-modal'>
+      <span>Sign in with liquid access</span>
+      <button>Connect wallet</button>
     </div>
   `
-
   document.body.appendChild(modal);
+  setStyles(frameStyles)
+}
+
+export const setStyles = (stylesObject: object) => {
+  const range = document.createRange();
+  let res = "";
+  Object.entries(stylesObject).forEach(([key]) => {
+    // @ts-ignore
+    const obj = JSON.stringify(stylesObject[key], null, "\t")
+      .replace(/"/g, "")
+      .replace(/,\n/g, ";")
+      .replace(/\}/g, ";}");
+    res = `<style>.${key}${obj},</style>`;
+  });
+  const frag = range.createContextualFragment(res);
+  // @ts-ignore
+  document.querySelector("head").append(frag);
 };
---
+++ src/components/frame/styles.ts
@@ -0,0 +1,19 @@
+export const frameStyles = {
+  // backdrop: {
+  //   position: 'fixed',
+  //   'z-index': 1,
+  //   left: 0,
+  //   top: 0,
+  //   height: '100%',
+  //   width: '100%',
+  //   overflow: 'auto',
+  //   'background-color': 'rgba(0,0,0,0.4)'
+  // },
+  'frame-modal': {
+    'background-color': '#fff',
+    'border-radius': '5px',
+    margin: '0 auto',
+    padding: '20px',
+    width: '70%'
+  }
+}
