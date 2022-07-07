3cb9bd4526e055508e0b496f29318837cd6a02d3
happy-cutman <happy.cutman@gmail.com>
2022-07-07 11:47:22 +0300

    FIX | frame styles logic

--- src/components/frame/index.ts
+++ src/components/frame/index.ts
@@ -8,8 +8,8 @@ export const showFrame = () => {
   const modal = document.createElement('div')
   modal.innerHTML = `
     <div class='frame-modal' id='frame-modal'>
-      <span>Sign in with liquid access</span>
-      <button>Connect wallet</button>
+      <span class='frame-text'>Sign in with liquid access</span>
+      <button class='frame-button'>Connect wallet</button>
     </div>
   `
   document.body.appendChild(modal);
@@ -19,14 +19,17 @@ export const showFrame = () => {
 export const setStyles = (stylesObject: object) => {
   const range = document.createRange();
   let res = "";
+  let arr: string[] = []
   Object.entries(stylesObject).forEach(([key]) => {
     // @ts-ignore
     const obj = JSON.stringify(stylesObject[key], null, "\t")
       .replace(/"/g, "")
       .replace(/,\n/g, ";")
       .replace(/\}/g, ";}");
-    res = `<style>.${key}${obj},</style>`;
+    arr.push(`.${key}${obj}`)
   });
+
+  res = `<style>${arr.join(' ')}</style>`;
   const frag = range.createContextualFragment(res);
   // @ts-ignore
   document.querySelector("head").append(frag);
--- src/components/frame/styles.ts
+++ src/components/frame/styles.ts
@@ -15,5 +15,22 @@ export const frameStyles = {
     margin: '0 auto',
     padding: '20px',
     width: '70%'
-  }
+  },
+  'frame-button': {
+    'background-color': '#ff4343',
+    padding: '4px 16px',
+    'border-radius': '5px',
+    color: '#fff',
+    cursor: 'pointer',
+    'text-align': 'center',
+    border: 'none'
+  },
+  'frame-button:hover': {
+    'background-color': '#de3838'
+  },
+  // 'frame-text': {
+  //   color: '#fff',
+  //   'font-size': '16px',
+  //   'margin-bottom': '16px'
+  // }
 }
