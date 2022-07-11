63aa37417f22fbb4774e092de91608bc92b0b42d
happy-cutman <happy.cutman@gmail.com>
2022-07-11 12:13:00 +0300

    ADD | script for showing frame with react

--- src/components/frame/index.ts
+++ src/components/frame/index.ts
@@ -1,44 +1,60 @@
 import { frameStyles } from './styles';
 
 
+// export const showFrame = () => {
+//   // JSON.parse(localStorage.getItem('account') as string)
+//   if (document.getElementById('frame-modal')) return
+//   const modal = document.createElement('div')
+//
+//   const toggleHtml = () => {
+//     // @ts-ignore
+//     document.getElementById('frame-modal').style.display='none';
+//     // @ts-ignore
+//     document.getElementById('frame-modal-nft').style.display='block'
+//   };
+//
+//   modal.innerHTML = `
+//     <div class='frame-modal' id='frame-modal'>
+//       <span class='frame-text'>Sign in with liquid access</span>
+//       <div>
+//         <button id='frame-button' class='frame-button'>Connect wallet</button>
+//       </div>
+//     </div>
+//     <div class='frame-modal' id='frame-modal-nft' style='display: none'>
+//       <div class='frame-text'>Your subscribe 1 NFT</div>
+//       <div class='frame-text'>NFT merchant 1</div>
+//       <button id='frame-button' class='frame-button'>Confirm</button>
+//     </div>
+//   `
+//   document.body.appendChild(modal);
+//   setStyles(frameStyles)
+//
+//   // @ts-ignore
+//   document.getElementById('frame-button').addEventListener("click", () => toggleHtml(), {once: true});
+//   // @ts-ignore
+//   // document.getElementById('frame-button').removeAttribute("onclick");
+//
+//   // // @ts-ignore
+//   // document.getElementById("frame-button").addEventListener("click", () => getWalletConnection());
+//   // // @ts-ignore
+//   // document.getElementById("frame-button").removeEventListener("click", () => getWalletConnection());
+// }
+
 export const showFrame = () => {
-  // JSON.parse(localStorage.getItem('account') as string)
-  if (document.getElementById('frame-modal')) return
-  const modal = document.createElement('div')
+  if (document.getElementById('la-frame')) return
+  const frame = document.createElement('div')
+  frame.innerHTML = `<div id="la-frame" class='la-frame'></div>`
+  document.body.appendChild(frame)
 
-  const toggleHtml = () => {
-    // @ts-ignore
-    document.getElementById('frame-modal').style.display='none';
-    // @ts-ignore
-    document.getElementById('frame-modal-nft').style.display='block'
-  };
-
-  modal.innerHTML = `
-    <div class='frame-modal' id='frame-modal'>
-      <span class='frame-text'>Sign in with liquid access</span>
-      <div>
-        <button id='frame-button' class='frame-button'>Connect wallet</button>
-      </div>
-    </div>
-    <div class='frame-modal' id='frame-modal-nft' style='display: none'>
-      <div class='frame-text'>Your subscribe 1 NFT</div>
-      <div class='frame-text'>NFT merchant 1</div>
-      <button id='frame-button' class='frame-button'>Confirm</button>
-    </div>
-  `
-  document.body.appendChild(modal);
-  setStyles(frameStyles)
-
-  // @ts-ignore
-  document.getElementById('frame-button').addEventListener("click", () => toggleHtml(), {once: true});
-  // @ts-ignore
-  // document.getElementById('frame-button').removeAttribute("onclick");
-
-  // // @ts-ignore
-  // document.getElementById("frame-button").addEventListener("click", () => getWalletConnection());
-  // // @ts-ignore
-  // document.getElementById("frame-button").removeEventListener("click", () => getWalletConnection());
-}
+  if (document.getElementById('la-frame')) {
+    const link = document.createElement('link')
+    const script = document.createElement('script')
+    link.setAttribute('href', './widget/index.css')
+    link.setAttribute('rel', 'stylesheet')
+    script.setAttribute('src', './widget/index.js')
+    document.body.append(link, script)
+  }
+};
 
 export const setStyles = (stylesObject: object) => {
   const range = document.createRange();
