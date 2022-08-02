49d4447bb11284b731c699d3e38d606506984bd0
happy-cutman <happy.cutman@gmail.com>
2022-08-02 13:05:11 +0300

    EDIT | link crypting logic

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,20 +1,28 @@
-import AES from 'crypto-js/aes';
 
 const id = () => Math.floor(Math.random() * 1011)
 
-const encryptParams = (str: string) => {
-  return AES.encrypt(JSON.stringify({ str }), 'secret').toString();
-}
+const cryptUrlParams = (key: string, text: string) => {
+  const textToChars = (text: string) => text.split("").map((c) => c.charCodeAt(0));
+  const byteHex = (n: any) => ("0" + Number(n).toString(16)).substr(-2);
+  const applyKeyToChar = (code: any) => textToChars(key).reduce((a, b) => a ^ b, code);
+
+  return text
+    .split("")
+    .map(textToChars)
+    .map(applyKeyToChar)
+    .map(byteHex)
+    .join("");
+};
 
 export const getUrl = (activate?: string) => {
   let urlParams = `userId=${id()}&merchantId=1`
 
-  const encryptedLink = encryptParams(urlParams)
+  const encryptedLink = cryptUrlParams('secretKey', urlParams)
 
   if (activate) {
     return `https://la-dashboard.vercel.app/_*${encryptedLink}&activate`;
   }
 
-  return `https://la-dashboard.vercel.app/_*${encryptedLink}&mint`;
-  // return `http://localhost:3001/_*${encryptedLink}&mint`;
+  // return `https://la-dashboard.vercel.app/_*${encryptedLink}&mint`;
+  return `http://localhost:3000/_*${encryptedLink}&mint`;
 }
